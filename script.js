31b17132b1346d97ac05f0ba643fab0592cb7d23
Aiken AU <123994071+aikenau@users.noreply.github.com>
2023-11-28 19:00:04 +0000

    upload the content

---
+++ script.js
@@ -0,0 +1,19 @@
+let sections = document.querySelectorAll('section');
+
+function handleScroll() {
+    let top = window.scrollY;
+    
+    sections.forEach(sec => {
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let startAnimationOffset = 150;
+
+        if (top >= offset - startAnimationOffset && top < offset + height) {
+            sec.classList.add('show-animate');
+        } else {
+            sec.classList.remove('show-animate');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
